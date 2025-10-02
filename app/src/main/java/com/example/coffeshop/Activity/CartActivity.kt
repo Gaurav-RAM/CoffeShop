8e8a7bd852cc982bc11bf7817e2c9eaeaca9a423
Gaurav <gauravghuraiya01@gmail.con>
2025-10-02 19:25:23 +0530

    Added complete cart holder and view cart functionality

--- app/src/main/java/com/example/coffeshop/Activity/CartActivity.kt
+++ app/src/main/java/com/example/coffeshop/Activity/CartActivity.kt
@@ -5,17 +5,67 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coffeshop.Adapter.CartAdapter
+import com.example.coffeshop.Helper.ChangeNumberItemsListener
+import com.example.coffeshop.Helper.ManagmentCart
 import com.example.coffeshop.R
+import com.example.coffeshop.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityCartBinding
+    lateinit var  managmentCart: ManagmentCart
+    private var tax: Double=0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cart)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        managmentCart= ManagmentCart(this)
+
+        calculatorCart()
+        setVariable()
+        initCartList()
+
+    }
+
+    private fun  initCartList() {
+        binding.apply {
+         listView.layoutManager=
+             LinearLayoutManager(this@CartActivity, LinearLayoutManager.VERTICAL,false)
+            listView.adapter= CartAdapter(
+                managmentCart.getListCart(),
+                this@CartActivity,
+                object : ChangeNumberItemsListener {
+                        override fun onChanged(){
+                            calculatorCart()
+                    }
+
+                }
+            )
         }
+    }
+
+
+    private fun  setVariable() {
+        binding.backBtn.setOnClickListener { finish() }
+    }
+
+    private fun  calculatorCart() {
+        val  percentTax=0.02
+        val delivery=15
+        tax= ((managmentCart.getTotalFee()*percentTax)*100)/100.0
+        val total= ((managmentCart.getTotalFee()+tax+delivery)*100)
+        val itemtotal= (managmentCart.getTotalFee()*100)/100
+        binding.apply {
+            totalFeeTxt.text="$$itemtotal"
+            totalTaxTxt.text="$$tax"
+            deliveryTxt.text="$$delivery"
+            totalTxt.text="$$total"
+
+        }
+
     }
 }
